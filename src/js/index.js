339125c73d59e1ddec8fd8dab69edfa3d110e72d
allenclove <1393623445@qq.com>
2021-09-29 01:26:35 +0800

    引入iconv-lite模块，解决获取cmd输出中文乱码问题

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,19 @@
+const iconv = require('iconv-lite');
 const exec = require('child_process').exec;
 
 
-var cmd = 'wmic process get executablepath | find "WeChatApp.exe"';
+var cmd = 'wmic process get executablepath | find "LeagueClientUx.exe"';
 var btn = document.getElementById("btn");
 var p = document.getElementById("pdemo");
 
 btn.onclick = () => {
   console.log("haha")
   try {
-    exec(cmd, function (err, stdout, stderr) {
+    exec(cmd, { encoding: 'buffer' }, function (err, stdout, stderr) {
         // 获取命令行执行的输出
-        console.log(stdout);
-        var arr = stdout.split("\r\r\n");
+        var stdoutStr = iconv.decode(stdout, 'cp936');
+        console.log(stdoutStr);
+        var arr = stdoutStr.split("\r\r\n");
         let newArr = arr.filter(i=>i && i.trim()).filter(i=>i.trim());
         console.log(newArr);
         p.innerHTML = newArr;
