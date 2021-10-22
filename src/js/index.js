b89cfa232ce22ff621877fe6bacfd1cfdb6f63f0
huangjianlin <3335433588@qq.com>
2021-10-22 17:17:05 +0800

    style: 按钮样式

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,24 @@
-const iconv = require('iconv-lite');  //用于解决中文输出乱码
-const fs = require('fs');  //用于调用本地读取文件的api
-const axios = require('axios');  //用于异步请求
-const exec = require('child_process').exec;  //用于执行cmd命令
+const iconv = require('iconv-lite'); //用于解决中文输出乱码
+const fs = require('fs'); //用于调用本地读取文件的api
+const axios = require('axios'); //用于异步请求
+const exec = require('child_process').exec; //用于执行cmd命令
 const path = require('path'); //处理文件路径
-const async = require('async');  //用来实现同步运行方法
+const async = require('async'); //用来实现同步运行方法
 const { resolve } = require('path');
 
 
 
-var port;  //端口
-var username;  //用户名,默认riot
-var password;  //密码
-var protocol;  //协议,一般是https
+var port; //端口
+var username; //用户名,默认riot
+var password; //密码
+var protocol; //协议,一般是https
 //该cmd命令需要管理员权限才能保证获取到路径
-var getRunStatusCMD = 'wmic process get name | find "LeagueClientUx.exe"';  //用于检测游戏是否运行
-var getPathCMD = 'wmic process get executablepath | find "LeagueClientUx.exe"';  //用于获取游戏启动路径
-var auto_acceptance_run_status = false;  //这个作为自动接受功能是否开启的标志, 默认为false
-var lol_run_status = false;  //这个作为游戏是否运行的标志
-var is_parse_lockfile = false;  //这个是是否解析lockfile文件的标志
-var lolpath = '';  //游戏启动目录路径
+var getRunStatusCMD = 'wmic process get name | find "LeagueClientUx.exe"'; //用于检测游戏是否运行
+var getPathCMD = 'wmic process get executablepath | find "LeagueClientUx.exe"'; //用于获取游戏启动路径
+var auto_acceptance_run_status = false; //这个作为自动接受功能是否开启的标志, 默认为false
+var lol_run_status = false; //这个作为游戏是否运行的标志
+var is_parse_lockfile = false; //这个是是否解析lockfile文件的标志
+var lolpath = ''; //游戏启动目录路径
 var intervalID; //存储自动接受循环的id
 
 
@@ -43,27 +43,31 @@ async function progremInit() {
 
 //开启按钮
 var openBtn = document.getElementById("openBtn");
-openBtn.onclick = async () => {
+openBtn.onclick = async() => {
     if (!auto_acceptance_run_status) {
         //开启自动接受
         pmessage.innerHTML = '<font style = "color:red">自动接受已开启</font>';
         openBtn.innerHTML = 'close';
+        // 修改按钮dom的class名
+        openBtn.className = 'el-button el-button--primary';
         auto_acceptance_run_status = true;
 
         await cycleCall();
-    }else{
+    } else {
         //关闭自动接受
         pmessage.innerHTML = '<font style = "color:black">自动接受已关闭</font>';
         openBtn.innerHTML = 'open';
+        // 修改按钮dom的class名
+        openBtn.className = 'el-button el-button--info';
         window.clearInterval(intervalID);
         auto_acceptance_run_status = false;
     }
 }
 
-var searchState;  //这个是游戏对局状态
+var searchState; //这个是游戏对局状态
 function cycleCall() {
     return new Promise(resolve => {
-        intervalID = window.setInterval(async () => {
+        intervalID = window.setInterval(async() => {
             var res = await callLOLApi('get', '/lol-lobby/v2/lobby/matchmaking/search-state');
             console.log(res.status);
             if (res.status !== 200) {
@@ -84,14 +88,14 @@ function cycleCall() {
 function islolRunning() {
     return new Promise(resolve => {
         try {
-            exec(getRunStatusCMD, { encoding: 'buffer' }, function (err, stdout, stderr) {
+            exec(getRunStatusCMD, { encoding: 'buffer' }, function(err, stdout, stderr) {
                 // 获取命令行执行的输出并解析
                 var stdoutStr = iconv.decode(stdout, 'cp936');
                 var arr = stdoutStr.split("\r\r\n");
                 let newArr = arr.filter(i => i && i.trim()).filter(i => i.trim()); //过滤为空的字符串
                 var lolAppName = newArr[0]; //获取到了进程名则说明游戏正在运行
 
-                if (typeof (lolAppName) != 'undefined' && lolAppName.trim() != '') {  //这里需要利用短路功能
+                if (typeof(lolAppName) != 'undefined' && lolAppName.trim() != '') { //这里需要利用短路功能
                     //游戏启动了,进行下一步获取游戏路径
                     console.log(lolAppName);
                     resolve(true);
@@ -109,7 +113,7 @@ function islolRunning() {
 //获取游戏启动路径
 function getlolRunPath() {
     try {
-        exec(getPathCMD, { encoding: 'buffer' }, function (err, stdout, stderr) {
+        exec(getPathCMD, { encoding: 'buffer' }, function(err, stdout, stderr) {
             // 获取命令行执行的输出并解析
             var stdoutStr = iconv.decode(stdout, 'cp936');
             var arr = stdoutStr.split("\r\r\n");
@@ -125,11 +129,11 @@ function getlolRunPath() {
 }
 
 //读取lockfile文件: LeagueClient:16408:51892:H60XiFdnlzAiR9xlJDuCwQ:https
-function parseLockFile(dirPath, flag) {  //这里第二个参数是:是否强制重新读取lockfile
+function parseLockFile(dirPath, flag) { //这里第二个参数是:是否强制重新读取lockfile
     console.log('dirPath:' + dirPath);
     if (flag || !is_parse_lockfile) {
         //当is_parse_lockfile标志为flase说明是第一次解析,或者强制重新读取标志为true
-        let temp = fs.readFileSync(dirPath);  //这个相对位置是从项目根目录开始
+        let temp = fs.readFileSync(dirPath); //这个相对位置是从项目根目录开始
         let mess = iconv.decode(temp, 'cp936'); //输出中文用这个编码
         var messList = mess.split(':');
 
